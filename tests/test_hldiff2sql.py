80672b14d0d5d7421f004c643e5a68a68bdc331b
eugenefil <fil.eugene@gmail.com>
2017-06-13 10:38:07 +0200

    Converts updated rows to sql

--- tests/test_hldiff2sql.py
+++ tests/test_hldiff2sql.py
@@ -1,6 +1,8 @@
 # TODO: tests
+# two updated columns
+# different updated columns in different rows
+# updated row with --> action tag
 # typed header
-# updated row
 # empty input
 # ignore context rows
 # multiple changes
@@ -23,9 +25,29 @@ os.environ['PATH'] = (
 )
 
 
-def run(input):
+class RunError(subprocess.CalledProcessError):
+    """Raised when process returned non-zero exit status."""
+
+    def init(self, returncode, cmd, output=None, stderr=None):
+        super().init(returncode, cmd, output=output)
+        self.stderr = stderr
+
+    def __str__(self):
+        msg = super().__str__()
+        return "%s Captured stderr:\n%s" % (msg, self.stderr)
+
+
+def run(args, input):
+    """Run process, pipe input to it and return its captured output.
+
+    Output is returned as a tuple (stdout, stderr). If program exits
+    with non-zero code, throw RunError exception.
+    """
+    # use binary streams (universal_newlines=False) and encode/decode
+    # manually, otherwise \r\n in returned query string fields gets
+    # converted to \n, i.e. data gets corrupted
     p = subprocess.Popen(
-        ['hldiff2sql', 't'],
+        args,
         stdin=subprocess.PIPE,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE
@@ -34,13 +56,17 @@ def run(input):
         s.decode('utf-8')
         for s in p.communicate(input.encode('utf-8'))
     ]
+
+    if p.returncode != 0:
+        raise RunError(p.returncode, p.args, output=out, stderr=err)
+
     return out, err
 
 
 def convert(input_rows):
     f = StringIO()
     csv.writer(f).writerows(input_rows)
-    out, err = run(input=f.getvalue())
+    out, err = run(['hldiff2sql', 't'], input=f.getvalue())
     return list(csv.reader(StringIO(out), delimiter='\t'))
 
 
@@ -67,4 +93,15 @@ def test_deleted_row():
         ['id', 'name'],
         ['1', 'john'],
         ['2', 'bill']
+    ]
+
+
+def test_updated_row():
+    assert convert([
+        ['@@', 'id', 'name', 'age'],
+        ['->', '1', 'john->bill', '50->60']
+    ]) == [
+        ['update t set name = ?, age = ? where id = ? and name = ? and age = ?'],
+        ['name', 'age', 'id', 'name', 'age'],
+        ['bill', '60', '1', 'john', '50']
     ]
