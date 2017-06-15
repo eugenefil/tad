5c96bc44deab148a28fdee4ea54fdce1a4b8cc09
eugenefil <fil.eugene@gmail.com>
2017-06-15 20:13:52 +0200

    Moved common test functionality into testutil.py

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,4 +1,3 @@
-import subprocess
 import csv
 from io import StringIO
 import os
@@ -6,6 +5,8 @@ import os.path as path
 import shutil
 
 import pytest
+
+from testutil import run
 
 
 DBPATH = 'vfpdb/db.dbc'
@@ -16,49 +17,6 @@ rootdir = pytest.config.rootdir
 os.environ['PATH'] = (
     str(rootdir.join('tad')) + ':' + os.environ['PATH']
 )
-
-
-class RunError(subprocess.CalledProcessError):
-    """Raised when process returned non-zero exit status."""
-
-    def init(self, returncode, cmd, output=None, stderr=None):
-        super().init(returncode, cmd, output=output)
-        self.stderr = stderr
-
-    def __str__(self):
-        msg = super().__str__()
-        return "%s Captured stderr:\n%s" % (msg, self.stderr)
-
-
-def run(args, input=None):
-    """Run process, pipe input to it and return its captured output.
-
-    Output is returned as a tuple (stdout, stderr). If program exits
-    with non-zero code, throw RunError exception.
-    """
-    stdin = None
-    if not input is None:
-        input = input.encode('utf-8')
-        stdin = subprocess.PIPE
-
-    # use binary streams (universal_newlines=False) and encode/decode
-    # manually, otherwise \r\n in returned query string fields gets
-    # converted to \n, i.e. data gets corrupted
-    p = subprocess.Popen(
-        args,
-        stdin=stdin,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
-    )
-    out, err = [
-        s.decode('utf-8')
-        for s in p.communicate(input)
-    ]
-
-    if p.returncode != 0:
-        raise RunError(p.returncode, p.args, output=out, stderr=err)
-
-    return out, err
 
 
 def diff(db1dir, db2dir, table1, table2=None, typed_header=False):
--- tests/test_diff2sql.py
+++ tests/test_diff2sql.py
@@ -1,14 +1,15 @@
 # TODO: tests
+# group same updates
 # wrong diff format
 # spaces in column names
 
-import subprocess
 import csv
 from io import StringIO
 import os
-import os.path as path
 
 import pytest
+
+from testutil import run, RunError
 
 
 # add path to diff2sql to PATH
@@ -16,44 +17,6 @@ rootdir = pytest.config.rootdir
 os.environ['PATH'] = (
     str(rootdir.join('tad')) + ':' + os.environ['PATH']
 )
-
-
-class RunError(subprocess.CalledProcessError):
-    """Raised when process returned non-zero exit status."""
-
-    def init(self, returncode, cmd, output=None, stderr=None):
-        super().init(returncode, cmd, output=output)
-        self.stderr = stderr
-
-    def __str__(self):
-        msg = super().__str__()
-        return "%s Captured stderr:\n%s" % (msg, self.stderr)
-
-
-def run(args, input):
-    """Run process, pipe input to it and return its captured output.
-
-    Output is returned as a tuple (stdout, stderr). If program exits
-    with non-zero code, throw RunError exception.
-    """
-    # use binary streams (universal_newlines=False) and encode/decode
-    # manually, otherwise \r\n in returned query string fields gets
-    # converted to \n, i.e. data gets corrupted
-    p = subprocess.Popen(
-        args,
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
-    )
-    out, err = [
-        s.decode('utf-8')
-        for s in p.communicate(input.encode('utf-8'))
-    ]
-
-    if p.returncode != 0:
-        raise RunError(p.returncode, p.args, output=out, stderr=err)
-
-    return out, err
 
 
 def convert(input_rows, typed_header=False, delimiter='\t'):
